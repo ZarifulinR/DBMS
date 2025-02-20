eafeab211639ffa2e0649b65358544d4a4220b66
ASUTP <ASUTP@DESKTOP-SSAKK7M>
2025-02-20 10:24:17 +0300

    Hw Sarary DONE

--- T-SQL/SQLQuery2 Schedule test.sql
+++ T-SQL/SQLQuery2 Schedule test.sql
@@ -1,5 +1,11 @@
 USE PV_319_Import;
 GO
-EXEC sp_ScheduleForGroup N'PV_319', N'Объектно-ориент%',N'Ковтун', N'2024-06-01',N'18:30';
-EXEC sp_PrintScheduleForGroup N'PV_319';
+--EXEC sp_PrintScheduleForGroup N'PV_319';
+--EXEC sp_PrintScheduleForGroupPeriod N'PV_319','2024-06-01','2024-07-01';
 --SELECT * FROM Schedule;
+--SELECT =
+-- FROM sys.procedures
+-- WHERE name = 'sp_PrintScheduleForGroupPeriod';
+DECLARE @salary SMALLMONEY 
+EXEC @salary = sp_CountTeachersMonthSalary N'Ковтун',2024,06
+PRINT (@salary)
